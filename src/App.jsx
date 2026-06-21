f89f63329e20dd1970c2e243b0fda02c66012a67
FATIMA QURESHI <qfatima504@gmail.com>
2026-06-22 02:10:42 +0530

    Added macOS-style icons and zoom animation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+import "./app.scss"
+import Dock from './components/Dock';
+function App(){
+    return (
+        <main>
+            <Dock />
+        </main>
+    )
+}
+export default App
