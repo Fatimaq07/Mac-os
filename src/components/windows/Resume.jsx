ca32d0d8037442ebf6f2ca86cbb37e33b9b2cc63
FATIMA QURESHI <qfatima504@gmail.com>
2026-06-24 00:32:47 +0530

    created a separate Resume window

---
+++ src/components/windows/Resume.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import MacWindow from './MacWindow'
+import "./resume.scss"
+const Resume = () => {
+    return(
+       <MacWindow>
+       <div className="resume-window">
+    <embed
+        src="/fatima-resume.pdf"
+        title="Resume"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+    />
+</div>
+       </MacWindow>
+    )
+}
+
+export default Resume 
