84eb15df03a2f1dfd6f8b363b2123802f0ec54d3
FATIMA QURESHI <qfatima504@gmail.com>
2026-06-24 02:49:56 +0530

    Added Spotify playlist window

---
+++ src/components/windows/Spotify.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import MacWindow from './MacWindow';
+import "./spotify.scss"
+
+const Spotify = () => {
+  return (
+    <MacWindow width='25vw'>
+      <div className="spotify-window">
+        <iframe
+          data-testid="embed-iframe"
+          style={{ borderRadius: '12px' }}
+          src="https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M?utm_source=generator&theme=0"
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          title="Spotify Playlist"
+        />
+      </div>
+    </MacWindow>
+  );
+};
+
+export default Spotify;
